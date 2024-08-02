b328026d0946be3c6835f4fa35daef49b5cad951
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-08-02 22:00:39 +0545

    login completed

--- app/src/main/java/com/ratna/hungryhiveadmin/AdminDashboardActivity.java
+++ app/src/main/java/com/ratna/hungryhiveadmin/AdminDashboardActivity.java
@@ -1,8 +1,6 @@
 package com.ratna.hungryhiveadmin;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
-    Button adminLoginButton;
+public class AdminDashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        adminLoginButton = findViewById(R.id.adminLoginButton);
-        adminLoginButton.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, AdminDashboardActivity.class);
-            startActivity(intent);
+        setContentView(R.layout.activity_admin_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
