1d82e6730f779e2bc2b5cd29ffacf21b03446f2c
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-08-04 21:40:20 +0545

    initial commit

--- app/src/main/java/com/ratna/hungryhiveadmin/AddItemActivity.java
+++ app/src/main/java/com/ratna/hungryhiveadmin/AddItemActivity.java
@@ -1,24 +1,44 @@
 package com.ratna.hungryhiveadmin;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AddItemActivity extends AppCompatActivity {
+  ImageView selectImage;
+  TextView textSelectImage;
+
+  ActivityResultLauncher<PickVisualMediaRequest> pickImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_item);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        selectImage = findViewById(R.id.selectImage);
+        textSelectImage = findViewById(R.id.textSelectImage);
+
+        pickImage = registerForActivityResult(
+                new ActivityResultContracts.PickVisualMedia(),
+                uri -> {
+                    if (uri != null) {
+                        selectImage.setImageURI(uri);
+                    }
+                }
+        );
+
+        textSelectImage.setOnClickListener(view -> {
+            PickVisualMediaRequest request = new PickVisualMediaRequest.Builder()
+                    .setMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.INSTANCE)
+                    .build();
+            pickImage.launch(request);
         });
     }
 }
--- app/src/main/java/com/ratna/hungryhiveadmin/AdminDashboardActivity.java
+++ app/src/main/java/com/ratna/hungryhiveadmin/AdminDashboardActivity.java
@@ -1,24 +1,54 @@
 package com.ratna.hungryhiveadmin;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AdminDashboardActivity extends AppCompatActivity {
+CardView cardViewAddItem, cardViewAllItem, cardViewAdminProfile, cardViewCreateNewAdmin, CardViewOrderDispatch, CardViewUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cardViewAddItem = findViewById(R.id.cardViewAddItem);
+        cardViewAllItem = findViewById(R.id.cardViewAllItem);
+        cardViewAdminProfile = findViewById(R.id.cardViewAdminProfile);
+        cardViewCreateNewAdmin = findViewById(R.id.cardViewCreateNewAdmin);
+        CardViewOrderDispatch = findViewById(R.id.CardViewOrderDispatch);
+        CardViewUsers = findViewById(R.id.CardViewUsers);
+
+        cardViewAddItem.setOnClickListener(view -> {
+            Intent intent = new Intent(AdminDashboardActivity.this, AddItemActivity.class);
+            startActivity(intent);
+        });
+
+        cardViewAllItem.setOnClickListener(view -> {
+
+        });
+
+        cardViewAdminProfile.setOnClickListener(view -> {
+
+        });
+
+        cardViewCreateNewAdmin.setOnClickListener(view -> {
+
+        });
+
+        CardViewOrderDispatch.setOnClickListener(view -> {
+
+        });
+
+        CardViewUsers.setOnClickListener(view -> {
+
         });
     }
 }
