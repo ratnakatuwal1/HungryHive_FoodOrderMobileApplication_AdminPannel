c676f4bea58273968a5ef3f6df021f990122a31f
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-08-08 23:12:51 +0545

    initial commit

--- app/src/main/java/com/ratna/hungryhiveadmin/LoginActivity.java
+++ app/src/main/java/com/ratna/hungryhiveadmin/LoginActivity.java
@@ -3,6 +3,8 @@ package com.ratna.hungryhiveadmin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
     Button adminLoginButton;
+    EditText editTextEmailAddress, editTextPassword;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +26,34 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
         adminLoginButton = findViewById(R.id.adminLoginButton);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
+        mAuth = FirebaseAuth.getInstance();
+
         adminLoginButton.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, AdminDashboardActivity.class);
-            startActivity(intent);
+            String email = editTextEmailAddress.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+
+            if (!email.isEmpty() && !password.isEmpty()){
+                loginAdmin(email, password);
+            } else {
+                Toast.makeText(LoginActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginAdmin(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, AdminDashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            } else {
+                Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
