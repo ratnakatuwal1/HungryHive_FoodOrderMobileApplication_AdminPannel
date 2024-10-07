7f3d493a5f02d119c0c37eb76177cefd9cde9564
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-10-07 22:06:16 +0545

    initial commit

--- app/src/main/java/com/ratna/hungryhiveadmin/AllItemActivity.java
+++ app/src/main/java/com/ratna/hungryhiveadmin/AllItemActivity.java
@@ -3,20 +3,27 @@ package com.ratna.hungryhiveadmin;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ratna.hungryhiveadmin.Adapter.MenuItemAdapter;
 import com.ratna.hungryhiveadmin.Model.AllMenu;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class AllItemActivity extends AppCompatActivity {
-RecyclerView allItemRecyclerView;
-MenuItemAdapter adapter;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    ArrayList<AllMenu> menuList;
+    RecyclerView allItemRecyclerView;
+    MenuItemAdapter adapter;
 
 
     @Override
@@ -25,12 +32,42 @@ MenuItemAdapter adapter;
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_all_item);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        retrieveMenuItem();
+
         allItemRecyclerView = findViewById(R.id.allItemRecyclerView);
 
-        ArrayList<AllMenu> menuList = new ArrayList<>();
-        menuList.add(new AllMenu("Burger", "100", "Delicious burger", "Beef, Lettuce, Cheese", String.valueOf(R.drawable.burger)));
-        menuList.add(new AllMenu("Pizza", "200", "Cheesy pizza", "Cheese, Tomato, Olives", String.valueOf(R.drawable.pizza)));
+//        ArrayList<AllMenu> menuList = new ArrayList<>();
 
+//        adapter = new MenuItemAdapter(this, menuList);
+//        allItemRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        allItemRecyclerView.setAdapter(adapter);
+    }
+
+    private void retrieveMenuItem() {
+//        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference foodRef = firebaseDatabase.getReference().child("Menu");
+        foodRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                menuList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    AllMenu menuItem = dataSnapshot.getValue(AllMenu.class);
+                    if (menuItem != null) {
+                        menuList.add(menuItem);
+                    }
+                }
+                setAdapter();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void setAdapter () {
         adapter = new MenuItemAdapter(this, menuList);
         allItemRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         allItemRecyclerView.setAdapter(adapter);
